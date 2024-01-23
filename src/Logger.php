a27004303a4476b9a38251b6150534929147c248
Sascha Schneider <sascha.schneider@customerce.com>
2024-01-23 12:14:05 +0100

    adds css to logger

--- src/Logger.php
+++ src/Logger.php
@@ -4,18 +4,22 @@ namespace Application;
 
 final class Logger
 {
+    private const DEFAULT_STYLE = 'border: 1px solid #2f343f; padding: 10px; black: white; font-weight: bold;';
+
+    private string $customStyle = '';
+
     public function debug(mixed $var, mixed $var2 = null): void
     {
         $argsCount = func_num_args();
+
+        echo $this->applyStyle();
 
         if ($argsCount > 1) {
             $args = func_get_args();
 
             foreach ($args as $arg) {
                 if (is_array($arg) || is_object($arg)) {
-                    echo '  <xmp>' . PHP_EOL;
                     print_r($arg);
-                    echo '  </xmp>' . PHP_EOL;
                 } else {
                     echo $arg;
                 }
@@ -23,17 +27,33 @@ final class Logger
         }
         else {
             if (is_array($var) || is_object($var)) {
-                echo '  <xmp>' . PHP_EOL;
                 print_r($var);
-                echo '  </xmp>' . PHP_EOL;
             } else {
                 echo $var;
             }
         }
+
+        echo '</xmp>';
     }
 
     public function log(): void
     {
 
+    }
+
+    public function setCustomStyle(string $customStyle): void
+    {
+        $this->customStyle = $customStyle;
+    }
+
+    private function applyStyle(): string
+    {
+        $tagPattern = '<xmp style="%s">';
+
+        if ('' === $this->customStyle) {
+            return sprintf($tagPattern, self::DEFAULT_STYLE);
+        }
+
+        return sprintf($tagPattern, $this->customStyle);
     }
 }
