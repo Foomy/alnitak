042a8e6b465e53c9724c0a32f6332cf32037bd91
Sascha Schneider <sir.foomy@googlemail.com>
2024-01-24 03:37:19 +0100

    [alnitak-2] adds templating

--- config/autoloader.php
+++ config/autoloader.php
@@ -3,9 +3,11 @@
 use Application\Logger;
 use Application\Config;
 use Application\Router;
+use Application\Response;
 
 return [
-    Router::class => APPLICATION_PATH . '/src/Router.php',
-    Config::class => APPLICATION_PATH . '/src/Config.php',
-    Logger::class => APPLICATION_PATH . '/src/Logger.php',
+    Response::class => APPLICATION_PATH . '/src/Response.php',
+    Router::class   => APPLICATION_PATH . '/src/Router.php',
+    Config::class   => APPLICATION_PATH . '/src/Config.php',
+    Logger::class   => APPLICATION_PATH . '/src/Logger.php',
 ];
---
+++ src/Response.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Application;
+
+class Response
+{
+    public const HTTP_OK                    = '200';
+    public const HTTP_NOT_FOUND             = '404';
+    public const HTTP_INTERNAL_SERVER_ERROR = '500';
+}
